a25e0f8c20426306c875d3adc09cd5979d7409d3
Ana Raquel de Holanda <anaraquel00@gmail.com>
2025-05-03 16:11:28 -0300

    updated project

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("==============================");
+        System.out.println("Bem-vindo ao banco!");
+        System.out.println("Por favor, digite o número da agencia: ");
+        String agencia = scan.nextLine();
+        System.out.println("Por favor, digite o número da conta: ");    
+        int numeroConta = scan.nextInt();
+        scan.nextLine(); // Consome a quebra de linha deixada pelo nextInt
+        System.out.println("Por favor, digite o seu nome: ");
+        String nomeCliente = scan.nextLine();
+        scan.nextLine(); // Limpa o buffer do scanner
+        System.out.println("Por favor, digite o saldo: ");  
+        double saldo = scan.nextDouble();
+        System.out.println("Olá, " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque.");
+        System.out.println("==============================");
+        scan.close();
+    
     }
+
+   
 }
